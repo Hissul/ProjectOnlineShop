782c4faa5c80f4b785ada979d68a73ff64543ac2
Alexander <121170207+Hissul@users.noreply.github.com>
2025-04-09 13:32:17 +0300

    Добавлены страници : Контакты, О нас, На заказ Переделан Хэдер и Футер

---
+++ Client/Pages/Store/Contacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Pages.Store
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
